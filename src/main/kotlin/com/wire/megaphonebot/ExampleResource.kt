033b08d97ca3bbebdd91f95c1d58250f7f7ebd8e
yamilmedina <yamilmedina@users.noreply.github.com>
2022-10-25 19:51:23 +0200

    chore: base init project

---
+++ src/main/kotlin/com/wire/megaphonebot/ExampleResource.kt
@@ -0,0 +1,25 @@
+package com.wire.megaphonebot
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import javax.ws.rs.Consumes
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+
+
+@Path("/api/v1/example")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+class ExampleResource {
+
+    private val logger: Logger by lazy { LoggerFactory.getLogger(this::class.java) }
+
+    @GET
+    fun printHello(): Response {
+        logger.error(">>> hellooooooo !")
+        return Response.ok("HOOOLA!").build()
+    }
+}
